2cae09cd99ede71bbc3819f210090b723eb7bb22
Daniel256 <danielbykov256@gmail.com>
2025-10-06 19:05:25 +1300

    generateStaticParams 40, 39

--- src/app/property/[id]/page.jsx
+++ src/app/property/[id]/page.jsx
@@ -3,26 +3,18 @@ import {ArrowLeftIcon} from "lucide-react";
 import PropPageV1 from "@/app/property/[id]/_propPagev1/PropPageV1";
 import {prisma} from "@/lib/prisma";
 import {PROPERTY_PAGE_URI} from "@/app/property/const";
-import { unstable_cache } from 'next/cache';
+
+export async function generateStaticParams() {
+  return [{id: '40'}, {id: '39'}]
+}
 
 export default async function PropertyPage({params}) {
   const {id} = await params
 
-  const getPropertyData = unstable_cache(
-    async (id) => {
-      return prisma.property.findUnique({
-        where: {id: parseInt(id)},
-        include: {agent: true}
-      })
-    },
-    ['property-by-id', id],
-    {
-      revalidate: 60, // revalidate cache every 60 seconds
-      tags: ['property']
-    }
-  );
-
-  const propertyData = await getPropertyData(id);
+  const propertyData = await prisma.property.findUnique({
+    where: {id: parseInt(id)},
+    include: {agent: true}
+  })
 
   return (
     <div>
